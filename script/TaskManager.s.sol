c59a98a55b75d0be1cf3ac705107724f6bc320d0
AJTECH0001 <aladejamiudamilola@gmail.com>
2025-07-23 22:26:34 +0100

    update readme

--- script/TaskManager.s.sol
+++ script/TaskManager.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {TaskManager} from "../src/";
 
 contract CounterScript is Script {
     Counter public counter;
